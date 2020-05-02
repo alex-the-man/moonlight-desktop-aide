e961369e3ab6816aeb13806d2d141587726626b0
Alex Man <alex-the-man@users.noreply.github.com>
2020-05-02 01:21:10 -0700

    WIP

--- setup.py
+++ setup.py
@@ -26,6 +26,5 @@ setup(
         ":sys_platform=='win32'": ['pypiwin32', 'py2exe'],
         ":sys_platform=='darwin'": ['pyobjc-framework-Quartz>=6.2', 'py2app>=0.21']
     },
-    app=['moonlight-desktop/__main__.py'],
     **extra_options
 )
